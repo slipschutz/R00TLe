00104ea064c0b0f150d1b0b10219bd93535408ae
Sam Lipschutz <lipschutz@nscl.msu.edu>
2014-06-09 12:35:33 -0400

    Changed the Map file and the packer slightly for the eff. set up.  Now if the mapfile maps to ignore, the packer will ignore the channel.  Added a included path to R00TleLogon for the .rootrc files.  Added graphLE2.C which is the beginnings of a new version for the new event structure

--- shell/R00TLeLogon.sh
+++ shell/R00TLeLogon.sh
@@ -72,7 +72,7 @@ echo "Unix.*.Root.DynamicPath: .:${R00TLeInstall}/lib:\$(ROOTSYS)/lib" >> .rootr
 echo "Unix.*.Root.MacroPath: .:${R00TLeInstall}/scripts:${R00TLeInstall}/users/${1}/macros:\$(ROOTSYS)/macros" >> .rootrc
 
 echo "Rint.Logon: ${R00TLeInstall}/users/$1/rootlogon.C">>.rootrc
-
+echo "ACLiC.IncludePaths: -I$HOME/R00TLe/src/include">>.rootrc
 ###Make a rootlogon
 rm -f rootlogon.C
 
