51a879b03b2d52656713d53e2f58739853613e35
Sam Lipschutz <lipschutz@nscl.msu.edu>
2015-02-24 16:06:52 -0500

    Improved the GetEvtInfo Script.  Now it tails the end of the file to get information about how long the run was and such

--- shell/GetEvtInfo.sh
+++ shell/GetEvtInfo.sh
@@ -4,9 +4,13 @@ if [ $# -ne 1 ]; then
     echo "Need to give a run Number"
     exit
 fi
-
 runNum=$(printf "%04d" $1)
 theFile=${R00TLeEvtFilesPath}/run-${runNum}-00.evt
+echo "looking for $theFile"
+
 if [ -f $theFile ]; then
     /usr/opt/nscldaq/10.2-112/bin/dumper --source=file://$theFile | head -4 | tail -3
+    echo;echo;
+    /usr/opt/nscldaq/10.2-112/bin/dumper --source=file://$theFile --exclude=PHYSICS_EVENT,PHYSICS_EVENT_COUNT,INCREMENTAL_SCALERS | tail -3
+    
 fi
