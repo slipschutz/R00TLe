558d170adb5bc8f936ecaab68fad3e34086392fe
Sam Lipschutz <lipschutz@nscl.msu.edu>
2015-03-17 10:04:57 -0400

    Removed commented out lines in s800 settings

--- src/S800/S800Settings.cc
+++ src/S800/S800Settings.cc
@@ -28,62 +28,7 @@ void S800Settings::ReadSettings(){
   }
   fBadFile = set->GetValue("BadPad.File",defaultfile);
 
-  //fMatrixFile = set->GetValue("Gretina.Matrix.File",defaultfile);
-  //for (int icore = 0; icore < 4; icore++) {
-  //  fCoreCalibFile[icore] = set->GetValue(Form("Gretina.Core.%d.Calibration.File",icore),defaultfile);
-  //}
-
-  //fdet2hole.clear();
-  //fhole2det.clear();
-  //for(int det=0; det<7; det++){
-  //  int hole = set->GetValue(Form("Detector.%d",det),-1);
-  //  if (hole!=-1){
-  //    fdet2hole[det] = hole;
-  //    fhole2det[hole] = det;
-  //  }
-  //}
-
-  //fcry2spec.clear();
-  //fspec2cry.clear();
-  //for(int spec=0; spec<28; spec++){
-  //  int cry = set->GetValue(Form("Spectcl.%02d",spec),-1);
-  //  fcry2spec[cry] = spec;
-  //  fspec2cry[spec] = cry;
-  //}
-
 }
-
-//int Settings::Det2Hole(int det){
-//  if (fdet2hole.count(det)==1){
-//    return fdet2hole[det];
-//  } else {
-//    return -1;
-//  }
-//}
-
-//int Settings::Hole2Det(int hole){
-//  if (fhole2det.count(hole)==1){
-//    return fhole2det[hole];
-//  } else {
-//    return -1;
-//  }
-//}
-
-//int Settings::Cry2Spec(int cry){
-//  if (fcry2spec.count(cry)==1){
-//    return fcry2spec[cry];
-//  } else {
-//    return -1;
-//  }
-//}
-
-//int Settings::Spec2Cry(int spec){
-//  if (fspec2cry.count(spec)==1){
-//    return fspec2cry[spec];
-//  } else {
-//    return -1;
-//  }
-//}
 
 // void Settings::PrintSettings(){
 //   std::cout << "Crdc.File\t"     << fCalFile      << std::endl;
@@ -96,5 +41,4 @@ void S800Settings::ReadSettings(){
 //     std::cout << Form("Crdc.Y.Slope.%d\t",i)  << fySlope[i]  << std::endl;
 //   }
 //   std::cout << "BadPad.File\t"         << fBadFile    << std::endl;
-//   std::cout << "Gretina.Matrix.File\t" << fMatrixFile << std::endl;
 // }
